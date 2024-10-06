86628ae923373ad8ac10ec6f4373f108be1bebab
Tushar <sharmatushar936@gmail.com>
2024-10-06 12:38:22 +0530

    Add axios and fetching functionality

---
+++ src/conf/conf.ts
@@ -0,0 +1,4 @@
+const conf = {
+  Omdb_API_KEY: String(import.meta.env.VITE_OMDB_API_KEY),
+};
+export default conf;
---
+++ src/services/getServices.ts
@@ -0,0 +1,13 @@
+import axios from "axios";
+import conf from "../conf/conf";
+const api = axios.create({
+  baseURL: "https://www.omdbapi.com/",
+});
+
+export const GetMovieByName = (searchQuery: string) => {
+  return api.get(
+    `?apikey=${conf.Omdb_API_KEY}&s=${encodeURIComponent(
+      searchQuery
+    )}&type=movie`
+  );
+};
