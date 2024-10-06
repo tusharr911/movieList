04681ff56677488b468c5d5389f9d9e8ffe09e18
Tushar <sharmatushar936@gmail.com>
2024-10-07 00:48:28 +0530

    Fix movie details routes

--- src/main.tsx
+++ src/main.tsx
@@ -11,27 +11,34 @@ import {
 } from "react-router-dom";
 import Layout from "./Layout.tsx";
 import Login from "./pages/Login.tsx";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { store } from "./store/ReduxStore.ts";
 import { Provider } from "react-redux";
-const queryClient = new QueryClient();
+import { store } from "./store/ReduxStore.ts";
+import MovieDetails from "./pages/MovieDetails.tsx";
+
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import WatchList from "./pages/WatchList.tsx";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
-    <Route path="/" element={<Layout />}>
-      <Route path="" element={<App />} />
+    <Route path="/">
       <Route path="/signup" element={<SignUp />} />
       <Route path="/login" element={<Login />} />
+      <Route element={<Layout />}>
+        <Route index element={<App />} />
+        <Route path=":imdbID" element={<MovieDetails />} />
+        <Route path="/watchlist" element={<WatchList />} />
+      </Route>
     </Route>
   )
 );
 
+const queryClient = new QueryClient();
 createRoot(document.getElementById("root")!).render(
-  <QueryClientProvider client={queryClient}>
-    <StrictMode>
+  <StrictMode>
+    <QueryClientProvider client={queryClient}>
       <Provider store={store}>
         <RouterProvider router={router} />
       </Provider>
-    </StrictMode>
-  </QueryClientProvider>
+    </QueryClientProvider>
+  </StrictMode>
 );
